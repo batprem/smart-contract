4748fd84e7a3793cc5895a6b25b7313a69479673
pc@zrch.com <pc@zrch.com>
2021-12-16 01:48:58 +0700

    refactor: Refactor codes

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier: MIT //Add License
+// SPDX-License-Identifier: MIT
+// Add License
 // Store some information on Blockchain
 
-pragma solidity >=0.6.0 < 0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 // `contract` can be figured as `class` in OOP
 contract SimpleStorage {
@@ -26,25 +27,25 @@ contract SimpleStorage {
         string name;
     }
 
-    People public person = People({
-        favoriteNumber: 2,
-        name: "test"
-    });
+    People public person = People({favoriteNumber: 2, name: "test"});
 
     People[] public people; // Dynamic array People[1] for fixed
     mapping(string => uint256) public nameToFavoriteNumber; //mappig
 
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public returns (uint256) {
         favoriteNumber = _favoriteNumber;
+        return favoriteNumber;
         // uint256 test = 4;
     }
+
     //`view` read state of blockchain (does make transaction)
-    
-    function retrieve() public view returns(uint256){
+
+    function retrieve() public view returns (uint256) {
         return favoriteNumber;
     }
+
     //`pure` do some type of math must not have return
-    function retrieve_plus_1() public view returns(uint256){
+    function retrieve_plus_1() public view returns (uint256) {
         return favoriteNumber + 1;
     }
 
@@ -54,14 +55,7 @@ contract SimpleStorage {
         uint256 _favoriteNumber
     ) public {
         // Push a person to people
-        people.push(
-            People(
-                {
-                    favoriteNumber: _favoriteNumber,
-                    name: _name
-                }
-            )
-        );
+        people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
         // Add to mapping
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,27 +1,28 @@
-// SPDX-License-Identifier: MIT //Add License
-pragma solidity >=0.6.0 < 0.9.0;
+// SPDX-License-Identifier: MIT
+//Add License
+pragma solidity >=0.6.0 <0.9.0;
 
 import "./SimpleStorage.sol";
 
-contract StorageFactory is SimpleStorage{
+contract StorageFactory is SimpleStorage {
     // Deploy contract from another contract
     SimpleStorage[] public simpleStorageArray;
 
-    function createSimpleStorageContract() public{
+    function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
         // return address of contract SimpleStorage
     }
 
-    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
         // Store data into contract
         // required
         // Address
         // ABI (Application Binary Interface)
         SimpleStorage simpleStorage = SimpleStorage(
-            address(
-                simpleStorageArray[_simpleStorageIndex]
-            )
+            address(simpleStorageArray[_simpleStorageIndex])
         );
         simpleStorage.store(_simpleStorageNumber);
     }
@@ -29,9 +30,7 @@ contract StorageFactory is SimpleStorage{
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
         // Get stored data
         SimpleStorage simpleStorage = SimpleStorage(
-            address(
-                simpleStorageArray[_simpleStorageIndex]
-            )
+            address(simpleStorageArray[_simpleStorageIndex])
         );
         return simpleStorage.retrieve();
     }
