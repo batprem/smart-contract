229799a7424a2686aba4346969ab57dee964e9c7
pc@zrch.com <pc@zrch.com>
2021-12-17 02:08:08 +0700

    fix: remove unused contracts

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,21 +1,24 @@
-pragma solidity  >=0.7.0 <0.9.0;
+// pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+// contract SimpleCollectible is ERC721 {
+//     uint256 public tokenCounter;
 
-contract SimpleCollectible is ERC721{
-    uint256 public  tokenCounter;
-    constructor () public ERC721 ("test-prem", "Naja"){
-        tokenCounter = 1;
-    }
+//     constructor() public ERC721("test-prem", "Naja") {
+//         tokenCounter = 1;
+//     }
 
-    function createCollectible(string memory tokenURI) public returns (uint256){
-        uint256 newItemId = tokenCounter;
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        
-        tokenCounter = tokenCounter + 1;
+//     function createCollectible(string memory tokenURI)
+//         public
+//         returns (uint256)
+//     {
+//         uint256 newItemId = tokenCounter;
+//         _mint(msg.sender, newItemId);
+//         _setTokenURI(newItemId, tokenURI);
 
-        return newItemId;
-    }
-}
+//         tokenCounter = tokenCounter + 1;
+
+//         return newItemId;
+//     }
+// }
